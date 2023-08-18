6160ce920d0028f2db715c0d6ded2c293e4764ee
mgr514 <mgr514@mun.ca>
2023-08-18 14:45:48 -0230

    deleted unused function

--- javascript.js
+++ javascript.js
@@ -62,16 +62,9 @@ const addBook = () =>
   updateNode.addEventListener("click", () =>{
     new_book.read = !new_book.read 
     readNode.innerHTML = `Read: ${new_book.read ? "No" : "Yes"}`
-    //editBook = (new_book, bookTitle.value, bookAuthor.value)
     localStorage.setItem("books", JSON.stringify(books))
   })
 
-  /*let editNode = document.createElement("button")
-  editNode.classList.add("edit-book")
-  editNode.innerHTML = "Edit"
-  editNode.addEventListeneer("click", () => {
-    editForm(new_book)
-  })*/
 
   const deleteBook = (book)=>{
     const books = JSON.parse(localStorage.getItem("books"))
@@ -91,12 +84,6 @@ const addBook = () =>
     bookRead.value,
   )
 
-  /*const editBook = (newTitle, newAuthor) => {
-    book.title = newTitle
-    new.author = newAuthor
-    localStorage.setItem("books", JSON.stringify(books))
-  }*/
-
   console.log(new_book)
 
   books.push(new_book)
@@ -107,15 +94,10 @@ const addBook = () =>
   bookCard.appendChild(authorNode)
   bookCard.appendChild(readNode)
   bookCard.appendChild(updateNode)
-  //bookCard.appendChild(editNode)
   bookCard.appendChild(deleteNode)
   bookShelf.appendChild(bookCard)
 }
-/*const editForm = (book) => {
-  bookTitle.value = book.title
-  bookAuthor.value = book.author
-  overlay.style.display = "flex"
-}*/
+
 
 newBook.addEventListener ("click", () => {
   overlay.style.display = "flex"
